87f930df26afc958e2970fe63b1ceaf4355abfe2
EdinsonMelendez <desarrollofroend@gmail.com>
2025-04-13 23:30:32 -0500

    Actualización

--- backend/conexion.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/dbnotas');
-
-const objetodb = mongoose.connection
-
-objetodb.on('connected', ()=>{console.log('Conexion correcta a db')})
-objetodb.on('error', ()=>{console.log('Error en la conexion a la db')})
-
-module.exports = mongoose
